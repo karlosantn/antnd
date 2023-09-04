00141dac1567f66e469922dce36818eef1f27539
karlosantn <karlos.antn@gmail.com>
2023-09-04 09:51:11 -0300

    atualização do formulário

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ function RolarParaEsquerda() {
     Direita.style = "display:flex"
     Esquerda.style = "display:none"
 }
+
+function abrirWhatsapp() {
+      var nome = document.getElementById("nome").value;
+      var telefone = document.getElementById("telefone").value;
+      var email = document.getElementById("email").value;
+      var msg = document.getElementById("msg").value;
+      var url = "https://wa.me/+5531993152234?text="
+        + "*Formulário de Contato*" + "%0a"
+        + "%0a"
+        + "*Nome*: " + nome + "%0a"
+        + "*Telefone*: " + telefone + "%0a"
+        + "*E-mail*: " + email + "%0a"
+        + "*Mensagem*: " + msg;
+      window.open(url, '_blank').focus();
+    }
